ed7040acab40a470ca5e30e1898edd7ef6a44b8b
rafia9005 <rafia9005@gmail.com>
2023-06-07 15:21:12 +0700

    deployment

---
+++ README.md
@@ -0,0 +1,21 @@
+## rest api project
+
+![kurukuru-kururing](https://github.com/rafia9005/rest-full-api/assets/70046808/ec7554ba-4bdb-464d-8edc-a493bad7e31e)
+
+<center>
+  <h1>simple project rest api
+</center>
+how to use?
+
+1.install all package with run command npm i or npm install
+
+```bash
+npm i
+
+npm install
+```
+
+
+  
+<li>author</li>
+@rafia9005
---
+++ routes/notes.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+const dataFilePath = path.join(__dirname, "data/notes.json");
+
+router.use(express.json());
+router.post("/", (req, res) => {
+  const { title, author } = req.body;
+  fs.readFile(dataFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("An error occurred while reading the file.");
+    }
+    let posts = [];
+    if (data.length !== 0) {
+      posts = JSON.parse(data);
+    }
+    const id = posts.length + 1;
+    posts.push({ id, title, author });
+    fs.writeFile(dataFilePath, JSON.stringify(posts), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("An error occurred while saving the data.");
+      }
+      res.status(200).json({
+        message: "Data berhasil disimpan",
+        data: {
+          id,
+          title,
+          author,
+        },
+      });
+    });
+  });
+});
+
+router.get("/", function (req, res) {
+  fs.readFile(dataFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("An error occurred while reading the file.");
+    }
+
+    const posts = JSON.parse(data);
+
+    res.json({
+      success: true,
+      data: posts,
+    });
+  });
+});
+
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('An error occurred while reading the file.');
+      }
+  
+      let posts = JSON.parse(data);
+      const postIndex = posts.findIndex(post => post.id === id);
+  
+      if (postIndex === -1) {
+        return res.status(404).json({ message: 'Data not found.' });
+      }
+  
+      posts.splice(postIndex, 1);
+  
+      fs.writeFile(dataFilePath, JSON.stringify(posts), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('An error occurred while saving the data.');
+        }
+  
+        res.status(200).json({ message: 'Data berhasil dihapus.' });
+      });
+    });
+  });
+
+module.exports = router;
